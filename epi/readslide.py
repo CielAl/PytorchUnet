6d8796531d3cdc904324c813e7e7f062b5bfe6a9
CielAl <cielmercy@gmail.com>
2018-11-29 21:52:01 -0500

    massive refactor

--- epi/readslide.py
+++ epi/readslide.py
@@ -40,33 +40,32 @@ def getThumbByMag(osh,target_mag=10,base_mag=20,speed=False):
 	return output
 	 
 	
-#simplify the args by composite types
-def ndpiAdaptor(imgtype,imgdir,fname,file_ext,classes,mags,totals):
-	fullname = imgdir+'/'+os.path.basename(fname).replace("_mask.png",file_ext)
-	if(imgtype=="img"): #if we're looking at an img, it must be 3 channel, but cv2 won't load it in the correct channel order, so we need to fix that
-		#print("imgMode:"+fullname)
-		osh = OpenSlide(fullname)
-		io= getThumbByMag(osh,mags[0],mags[1])
-	else: #if its a mask image, then we only need a single channel (since grayscale 3D images are equal in all channels)
-		#print("maskMode:"+fname)
-		#io=cv2.imread(fname)[:,:,0]# multiclass mask
-		io = plt.imread(fname)# if use plt -> single channel img will not have the 3rd axis.[:,:,0]
-		for i,key in enumerate(classes): #sum the number of pixels, this is done pre-resize, the but proportions don't change which is really what we're after
-			totals[1,i]+=sum(sum(io==key))
+
+
+def defaultLabelMap(io):
+	#0 1 2 3 --> -1 0 1 2
+	io = io - 1;
 	return io
+#nameparser - return dict of func
+def defaultNameParser(fname,params):
+	imgdir = params.get('imgdir','.');
+	suffix = params.get('suffix',"_mask.png")
+	file_ext = params.get('file_ext','.png')
+	fullname = imgdir+'/'+os.path.basename(fname).replace(suffix,file_ext)
+	name = {}
+	name['img'] = fullname
+	name['mask'] = fname
+	return name
 	
-#simplify the args by composite types
-#return 3channel img for both mask and img ->simplify the case of shape and size for padding and patch extraction
-def pngAdaptor(imgtype,imgdir,fname,file_ext,classes,mags,totals):
-	fullname = imgdir+'/'+os.path.basename(fname).replace("_mask.png",file_ext)
-	if(imgtype=="img"): #if we're looking at an img, it must be 3 channel, but cv2 won't load it in the correct channel order, so we need to fix that
-		#print("imgMode:"+fullname)
-		io = plt.imread(fullname)
-	else: #if its a mask image, then we only need a single channel (since grayscale 3D images are equal in all channels)
-		#print("maskMode:"+fname)
-		#io = (plt.imread(fname)*255).astype(np.uint8)# if use plt -> single channel img will not have the 3rd axis.[:,:,0]
-		io = cv2.imread(fname)
-		for i,key in enumerate(classes): #sum the number of pixels, this is done pre-resize, the but proportions don't change which is really what we're after
-		#those semantic coupling is freakingly crazy
-			totals[1,i]+=sum(sum(io[:,:,0]==key))
+def ioAdaptor(name,imgtype,inputfun,params,totals,**kwargs):
+#inputfun-->function to read. Further abstraction or detail can be done outside of this wrapper 
+#name-->parsed name from outside
+	io = inputfun(name)
+	if(imgtype=="mask"): #if its a mask image, then we only need a single channel (since grayscale 3D images are equal in all channels)
+		if kwargs.get("labelChange",None) is not None:
+			#function instead of table, in case there are more logical operations.
+			io = kwargs["labelChange"](io)
+		#label count
+		for i,key in enumerate(params['classes']): #sum the number of pixels, this is done pre-resize, the but proportions don't change which is really what we're after
+			totals[1,i]+=sum(sum(io[:,:,0]==key))			
 	return io	
